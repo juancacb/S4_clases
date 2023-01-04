be121a40576ff2541eca5cb193d1f43f1b99ec14
User <User@ADMIN>
2023-01-03 19:12:16 -0500

    Agregar archivos de proyecto.

---
+++ S4_clases/vistas/inicio.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S4_clases.vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class inicio : ContentPage
+    {
+        public inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btniniciar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new registro(txtusuario.Text));
+
+        }
+    }
+}
---
+++ S4_clases/vistas/registro.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S4_clases.vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registro : ContentPage
+    {
+        public registro(string usuario)
+        {
+            InitializeComponent();
+            lbUsuario.Text = "USUARIO CONECTADO: " +usuario;
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double precio = Convert.ToDouble(txtprecio.Text);
+                int cantidad = Convert.ToInt32(txtCantidad.Text);
+
+                double valor = precio * cantidad; //(%12)
+                DisplayAlert("VALOR TOTAL DEL PRODUCTO", valor.ToString(), "Cerar");
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+            
+        }
+    }
+}
